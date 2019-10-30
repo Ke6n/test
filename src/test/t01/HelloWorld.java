61b90d3d5386795ffd6707ae00f66a2b6c96c1bf
Ke6n <lk8619@163.com>
2019-10-30 21:42:33 +0100

    Hello World!

---
+++ src/test/t01/HelloWorld.java
@@ -0,0 +1,7 @@
+package test.t01;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.printf("%c%c%c%c%c %c%c%c%c%c%c", 72, 101, 108, 108, 111, 87, 111, 114, 108, 100, 33);
+    }
+}
